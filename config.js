00f8a45a3138700ee357233aae4b6926a21bda3b
Peter MEUEL <peter@teriiehina.net>
2020-04-02 08:47:11 -1000

    First commit

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "title": "Covid19 Tahiti",
+  "name": "covid19_tahiti",
+  "description": "Suivi de l'évolution du Covid-19 en Polynésie française",
+  "baseUrl": 'https://covid19tahiti.com',
+  "defaultLocale": "fr",
+  "locales": [
+    {
+      "code": "fr",
+      "iso": "fr-FR",
+      "name": "Français",
+      "file": "fr.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
---
+++ content/2020-04-02_premier-cas-detecte.md
@@ -0,0 +1,11 @@
+---
+title: Premier cas détecté
+date: '2020-04-02T06:34:18.890Z'
+severity: partial-outage
+affectedsystems:
+  - api
+resolved: true
+---
+Premier cas de covid détecté en polynésie
+
+<!--- language code: fr -->
